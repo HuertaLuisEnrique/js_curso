9c656bc998ccf00dbdfc974f293f74f938ec767a
Luis Huerta <huerta.en97@gmail.com>
2024-05-02 16:36:21 -0700

    feature: Seccion 2 completada

---
+++ seccion_dos/assets/js/app.js
@@ -0,0 +1,22 @@
+    //aquí
+ 
+ let a = 10, 
+     b = 20, 
+     c = 30,  
+     d = 'Spiderman'
+     e = 'Hola '
+     x = a + b;
+
+     const saludo  = e + d;
+
+
+
+// console.log(saludo);
+// console.log('Todo nuestro archivo.js está cargado');    //final
+
+let outerHeight = '10500';
+const outerWidth = '50100';
+
+let miNombre = 'Luis';        //aquí
+
+
